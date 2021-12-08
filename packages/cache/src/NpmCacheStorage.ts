4ee2020b89eb37176ac6f929d949f1402abcc49c
shaulg <shaulgo@gmail.com>
2021-12-08 15:23:42 +0200

    1. Fix package.json copied to dist 2. Improve performance

--- packages/cache/src/NpmCacheStorage.ts
+++ packages/cache/src/NpmCacheStorage.ts
@@ -1,12 +1,13 @@
 import path from "path";
-import execa from "execa";
 import fs from "fs-extra";
 import globby from "globby";
 
 import { NpmCacheStorageOptions } from "backfill-config";
 import { Logger } from "backfill-logger";
 
+import packageJson from "package-json";
 import { CacheStorage } from "./CacheStorage";
+import { npmInstall, npmPublish } from "./npm";
 
 export class NpmCacheStorage extends CacheStorage {
   constructor(
@@ -16,49 +17,64 @@ export class NpmCacheStorage extends CacheStorage {
     cwd: string
   ) {
     super(logger, cwd);
+    this.publishedVersions = NpmCacheStorage.getPublishedVersions(
+      options.npmPackageName
+    );
+  }
+
+  private static getPublishedVersions(packageName: string) {
+    return packageJson(packageName, {
+      allVersions: true,
+    }).then(({ versions }: { versions: {} }) => versions || {});
+  }
+
+  private readonly publishedVersions: Promise<Record<string, any>>;
+
+  private getLocalCacheFolder(hash: string): string {
+    return path.resolve(this.cwd, this.internalCacheFolder, "npm", hash);
+  }
+
+  private static async copy(src: string, dest: string) {
+    await fs.mkdirp(path.dirname(dest));
+    await fs.copy(src, dest);
+  }
+
+  private async isPublished(version: string) {
+    try {
+      const versions = await this.publishedVersions;
+      return !!versions[version];
+    } catch {
+      return false;
+    }
   }
 
   protected async _fetch(hash: string): Promise<boolean> {
-    const { npmPackageName, registryUrl, npmrcUserconfig } = this.options;
+    const { npmPackageName, npmrcUserconfig } = this.options;
 
-    const temporaryNpmOutputFolder = path.resolve(
-      this.cwd,
-      this.internalCacheFolder,
-      "npm",
-      hash
-    );
+    const rootCacheFolder = this.getLocalCacheFolder(hash);
 
-    const packageFolderInTemporaryFolder = path.join(
-      temporaryNpmOutputFolder,
+    const packageFolder = path.join(
+      rootCacheFolder,
       "node_modules",
       npmPackageName
     );
 
-    if (!fs.existsSync(packageFolderInTemporaryFolder)) {
-      fs.mkdirpSync(temporaryNpmOutputFolder);
+    if (!fs.existsSync(packageFolder)) {
+      fs.mkdirpSync(rootCacheFolder);
 
       try {
-        const runner = execa("npm", [
-          "install",
-          "--prefix",
-          temporaryNpmOutputFolder,
-          `${npmPackageName}@0.0.0-${hash}`,
-          "--registry",
-          registryUrl,
-          "--prefer-offline",
-          "--ignore-scripts",
-          "--no-shrinkwrap",
-          "--no-package-lock",
-          "--loglevel",
-          "error",
-          ...(npmrcUserconfig ? ["--userconfig", npmrcUserconfig] : []),
-        ]);
+        const runner = npmInstall({
+          prefix: rootCacheFolder,
+          packageName: npmPackageName,
+          version: `0.0.0-${hash}`,
+          npmrcUserconfig,
+        });
 
         this.logger.pipeProcessOutput(runner.stdout, runner.stderr);
 
         await runner;
       } catch (error) {
-        fs.removeSync(temporaryNpmOutputFolder);
+        fs.removeSync(rootCacheFolder);
 
         if (error.stderr.toString().indexOf("ETARGET") > -1) {
           return false;
@@ -69,79 +85,60 @@ export class NpmCacheStorage extends CacheStorage {
     }
 
     const files = await globby(`**/*`, {
-      cwd: packageFolderInTemporaryFolder,
+      cwd: packageFolder,
     });
 
     await Promise.all(
       files.map(async (file) => {
-        await fs.mkdirp(path.dirname(path.join(this.cwd, file)));
-        await fs.copy(
-          path.join(packageFolderInTemporaryFolder, file),
-          path.join(this.cwd, file)
-        );
+        if (!file.endsWith("package.json")) {
+          await NpmCacheStorage.copy(
+            path.join(packageFolder, file),
+            path.join(this.cwd, file)
+          );
+        }
       })
     );
-
     return true;
   }
 
   protected async _put(hash: string, filesToCache: string[]) {
-    const { npmPackageName, registryUrl, npmrcUserconfig } = this.options;
-
-    const temporaryNpmOutputFolder = path.resolve(
-      this.cwd,
-      this.internalCacheFolder,
-      "npm",
-      hash,
-      "upload"
-    );
-
-    // Create package.json file
-    fs.outputJSONSync(path.join(temporaryNpmOutputFolder, "package.json"), {
-      name: npmPackageName,
-      version: `0.0.0-${hash}`,
-    });
-
-    await fs.mkdirp(temporaryNpmOutputFolder);
-
-    await Promise.all(
-      filesToCache.map(async (file) => {
-        const destinationFolder = path.join(
-          temporaryNpmOutputFolder,
-          path.dirname(file)
-        );
-        await fs.mkdirp(destinationFolder);
-        await fs.copy(
-          path.join(this.cwd, file),
-          path.join(temporaryNpmOutputFolder, file)
-        );
-      })
-    );
-
-    // Upload package
-    try {
-      const runner = execa(
-        "npm",
-        [
-          "publish",
-          "--registry",
-          registryUrl,
-          "--loglevel",
-          "error",
-          ...(npmrcUserconfig ? ["--userconfig", npmrcUserconfig] : []),
-        ],
-        {
-          cwd: temporaryNpmOutputFolder,
-          stdout: "inherit",
-        }
+    const version = `0.0.0-${hash}`;
+    const isPublished = await this.isPublished(version);
+    if (!isPublished) {
+      const { npmPackageName, npmrcUserconfig } = this.options;
+      const temporaryNpmOutputFolder = path.resolve(
+        this.getLocalCacheFolder(hash),
+        "upload"
       );
 
-      this.logger.pipeProcessOutput(runner.stdout, runner.stderr);
+      // Create package.json file
+      fs.outputJSONSync(path.join(temporaryNpmOutputFolder, "package.json"), {
+        name: npmPackageName,
+        version,
+      });
+      await fs.mkdirp(temporaryNpmOutputFolder);
+      await Promise.all(
+        filesToCache.map(async (file) => {
+          await NpmCacheStorage.copy(
+            path.join(this.cwd, file),
+            path.join(temporaryNpmOutputFolder, file)
+          );
+        })
+      );
 
-      await runner;
-    } catch (error) {
-      if (error.stderr.toString().indexOf("403") === -1) {
-        throw new Error(error);
+      try {
+        const runner = npmPublish({
+          npmrcUserconfig,
+          cwd: temporaryNpmOutputFolder,
+        });
+
+        this.logger.pipeProcessOutput(runner.stdout, runner.stderr);
+
+        await runner;
+      } catch (error) {
+        if (error.stderr.toString().indexOf("403") === -1) {
+          throw new Error(error);
+        }
       }
     }
   }
